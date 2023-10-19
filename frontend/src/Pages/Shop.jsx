7eadf3b899130240dea21775651228c15f5dd78e
ChenAhrak <chenahrak@gmail.com>
2023-10-19 20:02:38 +0300

    init

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -5,7 +5,7 @@ import { Offers } from '../Components/Offers/Offers'
 import { NewCollections } from '../Components/NewCollections/NewCollections'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
-
+/////DFGdfgdgfd
 export const Shop = () => {
     return (
         <div>
