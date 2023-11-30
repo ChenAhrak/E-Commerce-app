13996bf0af029269c37628d4fe59a365945d035d
ChenAhrak <chenahrak@gmail.com>
2023-11-30 18:14:31 +0200

    mongo split

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const Users = require('./mongoDb')
 const cors = require('cors');
-const { Schema } = mongoose;
 const app = express()
 const port = 3001
-const mongoPort = "mongodb://localhost:27017/e-commerce-app"
 
 app.use(cors());
-
-mongoose.connect(mongoPort);
-//check if the connection with the database is successful
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose is connected!!!!');
-});
-
-// Create a schema for the products collection
-const usersSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  isAdmin: Boolean
-});
-
-//Create a model for the products collection
-const Users = mongoose.model('users', usersSchema);
-module.exports = Users;
 
 // //delete all products from the collection
 // Users.deleteMany({ name: "Eden"  })
@@ -35,24 +15,6 @@ module.exports = Users;
 //     console.error('Error deleting products:', error);
 //   });
 
-// app.get('/users', async (req, res) => {
-//   const dataFromClient = req.body;
-//   try {
-//     //check if email and password are exist in the database
-//     const userExist = await Users.findOne({ "$or": [{ email: dataFromClient.email }, { password: dataFromClient.password }] });
-//     if (!userExist) {
-//         res.json({ message: 'Email or Password are not exist go and sign up' });
-//     }
-//     else {
-//         res.json({ message: 'Email and Password are exist' });
-//     }
-   
-
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Server error');
-//   }
-// });
 
 app.use(express.json()); // To parse JSON in the request body
 
@@ -76,6 +38,7 @@ app.post('/signup', async (req, res) => {
       res.status(500).json({ message: 'Server error' });
   }
 });
+
 app.post('/login', async (req, res) => {
   const dataFromClient = req.body;
 
@@ -94,8 +57,6 @@ app.post('/login', async (req, res) => {
       res.status(500).json({ message: 'Server error' });
   }
 });
-
-
 
 
 
---
+++ backend/mongoDb.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const mongoPort = "mongodb://localhost:27017/e-commerce-app";
+mongoose.connect(mongoPort);
+//check if the connection with the database is successful
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected!!!!');
+});
+
+// Create a schema for the products collection
+const usersSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  isAdmin: Boolean
+});
+
+//Create a model for the products collection
+const Users = mongoose.model('users', usersSchema);
+module.exports = Users;
+
